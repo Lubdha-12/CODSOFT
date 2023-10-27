824d1897d637103104711a0910d4e82aaa41a9bc
Lubdha-12 <126138981+Lubdha-12@users.noreply.github.com>
2023-10-28 02:05:33 +0530

    Delete codsoft/UseGuessTheNumber.java

--- codsoft/UseGuessTheNumber.java
+++
@@ -1,9 +0,0 @@
-package codsoft;
-class UseGuessTheNumber //Driver Class
-{
-    public static void main(String[]args)
-    {
-        GuessTheNumber g=new GuessTheNumber();
-        g.guessNum();
-    }
-}
