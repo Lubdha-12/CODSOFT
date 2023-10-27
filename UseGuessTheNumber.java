b976c2a14751db31c7122d3cc7c0387a791d25ae
Lubdha-12 <126138981+Lubdha-12@users.noreply.github.com>
2023-10-28 01:40:59 +0530

    Add files via upload

---
+++ UseGuessTheNumber.java
@@ -0,0 +1,9 @@
+package codsoft;
+class UseGuessTheNumber //Driver Class
+{
+    public static void main(String[]args)
+    {
+        GuessTheNumber g=new GuessTheNumber();
+        g.guessNum();
+    }
+}
